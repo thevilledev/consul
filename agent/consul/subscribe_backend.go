212e7732837f1a9be0df8795c05252e64cf1f4f1
Daniel Nephin <dnephin@hashicorp.com>
2020-09-14 17:24:20 -0400

    Finish subscribe backend

--- agent/consul/subscribe_backend.go
+++ agent/consul/subscribe_backend.go
@@ -3,12 +3,14 @@ package consul
 import (
 	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/agent/consul/stream"
+	agentgrpc "github.com/hashicorp/consul/agent/grpc"
 	"github.com/hashicorp/consul/agent/subscribe"
 	"google.golang.org/grpc"
 )
 
 type subscribeBackend struct {
-	srv *Server
+	srv      *Server
+	connPool *agentgrpc.ClientConnPool
 }
 
 // TODO: refactor Resolve methods to an ACLBackend that can be used by all
@@ -22,13 +24,16 @@ var _ subscribe.Backend = (*subscribeBackend)(nil)
 // Forward requests to a remote datacenter by calling f if the target dc does not
 // match the config. Does nothing but return handled=false if dc is not specified,
 // or if it matches the Datacenter in config.
+//
+// TODO: extract this so that it can be used with other grpc services.
 func (s subscribeBackend) Forward(dc string, f func(*grpc.ClientConn) error) (handled bool, err error) {
 	if dc == "" || dc == s.srv.config.Datacenter {
 		return false, nil
 	}
-
-	// TODO: get grpc.ClientConn
-	var conn *grpc.ClientConn
+	conn, err := s.connPool.ClientConn(dc)
+	if err != nil {
+		return false, err
+	}
 	return true, f(conn)
 }
 
